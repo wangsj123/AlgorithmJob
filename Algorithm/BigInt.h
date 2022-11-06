7eb02139b0e4a6ae864362135812b1c481823f4e
anthonysong <anthonysong666@gmail.com>
2022-11-06 21:35:42 +0800

    add big int

---
+++ Algorithm/BigInt.h
@@ -0,0 +1,69 @@
+#include <string>
+#pragma once
+using namespace std;
+
+typedef struct BigInt //定义大整数
+{
+	int Length;
+	int Sign;		  //整数符号位,1为正数，-1为负数
+	string Value;
+
+	BigInt() {
+		this->Length = 0;
+		this->Sign = 1;
+		this->Value = "";
+	}
+
+	BigInt(char* s) {
+		this->Length = strlen(s);
+		this->Sign = +1;
+		if (s[0] == '-') {
+			this->Sign = -1;
+			this->Value = s[1];
+			this->Length -= 1;
+		}
+		else {
+			this->Value = s;
+		}
+
+	}
+
+	BigInt(string s) {
+		this->Length = s.size();
+		this->Sign = +1;
+		if (s[0] == '-') {
+			this->Sign = -1;
+			this->Value = s.substr(1, s.size());
+			this->Length -= 1;
+		}
+		else {
+			this->Value = s;
+		}
+
+	}
+
+
+}BigInt;
+
+int CmpBigInt(BigInt S, BigInt T) {
+	if (S.Sign == T.Sign) { //如果同号则比较值的大小
+
+		if (S.Value == T.Value) return 0;
+		if (S.Sign = 1) {
+			return S.Value.compare(T.Value);
+		}
+		else
+		{
+			return -S.Value.compare(T.Value);
+		}
+	}
+	else {
+		if (S.Sign - T.Sign == 2) { //异号则直接对比符号
+			return 1;
+		}
+		else {
+			return -1;
+		}
+
+	}
+}
